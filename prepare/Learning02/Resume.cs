34b859425d38b56114d4ec09f4f8fba0337d3dc7
suryiavishnusoranam <sor22003@byui.edu>
2023-01-28 15:35:25 -0700

    1/28

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -6,16 +6,16 @@ using System;
 
 public class Resume
 {
-    public string _name;
+    public string _vsname;
 
-    public List<Job> _jobs = new List<Job>();
+    public List<Job> _vsjobs = new List<Job>();
 
     public void Display()
     {
-        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Name: {_vsname}");
         Console.WriteLine("Jobs:");
 
-        foreach (Job job in _jobs)
+        foreach (Job job in _vsjobs)
         {
             job.Display();
         }
